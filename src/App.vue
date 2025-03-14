8c81e3f541ebf9ab4eb90056620f9e649b123c6c
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-03-14 22:15:41 +0400

    create footer but need a lot of fixs

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
   <AppHeader />
   <AppMain />
   <AppHighlights />
-
+  <AppFooter />
 </template>
 
 <script setup>
 import AppHeader from './views/AppHeader.vue'
 import AppMain from './views/AppMain.vue';
 import AppHighlights from './views/AppHighlights.vue';
+import AppFooter from './views/AppFooter.vue';
 </script>
---
+++ src/views/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="bg-black text-white">
+    <div class="list-none p-7">
+        <h1 class="font-bold text-3xl m-5">Rapid Rent</h1>
+        <li class="m-1">2730 W Briton Rd</li>
+        <li class="m-1">Oklahoma City</li>
+        <li class="m-1">OK 73120</li>
+        <li class="m-1"> United States</li>
+        <div class="p-6">
+            <a class="text-white" href=""><img src="../photos/logos/instagram (2).png" alt=""></a>
+
+        </div>
+    </div>
+  </footer>
+</template>
